6e63cd83fb3c864890fdf01e119ca33dcf993c55
124k3 <akha3743@gmail.com>
2024-09-14 10:18:51 -0700

    added method to take valid intInput + comments

--- GameLogic.java
+++ GameLogic.java
@@ -21,12 +21,49 @@ public class GameLogic
      * the random number {@code randomNumber >= lowerLimit}
      * @param upperLimit integet with which the ending point of the random 
      * limit is determined {@code randomNumber < upperLimit}
+     * @return {@code random.nextInt()} a random integet within the range 
+     * {@code lowerLimit >= randomNumber <upperLimit}
      *
      */
     public int getRandomNumber(Random random, int lowerLimit, int upperLimit)
     {
         return random.nextInt(lowerLimit, upperLimit);
 
+    }
+    
+    /**
+     * only return(s) a valid integet input
+     * 
+     * If the input obtained isn't valid it will again prompt the same
+     * untill and unless the suitable (valid) input is obtained.
+     * @param scanner the scanner object of the class {@code java.util.Scanner}
+     * @param prompt the prompt(message) to be shown while asking for the input.
+     * the prompt supports {@code C} style text formatting {@code %d, %n, %f..}
+     * @param errMessage the message to be shown if the input 
+     * obtained is of invalid type.
+     * @return returns a valid integer (gauranteed).
+     */
+    public int getValidIntInput(
+            Scanner scanner, String prompt, String errMessage)
+    {
+        int validIntInput = 0;
+        boolean invalidInput = true;
+        // assuming that the input obtained is invalid from the begining
+
+        while (invalidInput)
+        {
+            System.out.printf(prompt);
+            try
+            {
+                validInput = scanner.nextInt();
+                invalidInput = false;
+            }
+            catch (Exception e)
+            {
+                System.out.err(errMessage);
+            }
+        }
+        return validIntInput;
     }
 
 }
